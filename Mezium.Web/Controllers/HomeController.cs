fcc8be56b4455b8bfdc190bfa16107cbca5533e6
Dmitry Olshak <dima.olshak@mail.ru>
2021-12-07 12:22:32 +0300

    Added a stories list on home page

--- Mezium.Web/Controllers/HomeController.cs
+++ Mezium.Web/Controllers/HomeController.cs
@@ -15,7 +15,32 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var stories = new StoryViewModel[]
+        {
+            new StoryViewModel()
+            {
+                AuthorName = "Mark Desent",
+                Title = "You don’t need JWT anymore",
+                Description = "A simpler way to authenticate users with web3 using signed messages.",
+                PublishDate = DateOnly.FromDateTime(DateTime.Now)
+            },
+            new StoryViewModel()
+            {
+                AuthorName = "Miroslaw Shpak",
+                Title = "Why Senior Software Engineer Interviews Have Become So Damn Difficult",
+                Description = "There is a general sense that interviews for senior positions are getting harder all the time.",
+                PublishDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-2))
+            },
+            new StoryViewModel()
+            {
+                AuthorName = "Nick Nolan",
+                Title = "What Would Happen If You Bought $100 of Bitcoin Every Month For a Year?",
+                Description = "Here’s how much you’d have after a year.",
+                PublishDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-5))
+            }
+        };
+
+        return View(stories);
     }
 
     public IActionResult Privacy()
---
+++ Mezium.Web/Models/StoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mezium.Web.Models;
+
+public class StoryViewModel
+{
+    public string AuthorName { get; init; } = String.Empty;
+    public string Title { get; init; } = String.Empty;
+    public string Description { get; init; } = String.Empty;
+    public DateOnly PublishDate { get; init; }
+}
